d9dd491490af436d6a1b5240f0513794528f60e3
wignn <tigfiragnafatur1933@gmail.com>
2026-04-30 15:08:10 +0700

    fix: remove .env file and update allowedHosts in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   server: {
     port: 3000,
+    allowedHosts: ["novels.wign.cloud", "localhost"],
   },
+  
 });
